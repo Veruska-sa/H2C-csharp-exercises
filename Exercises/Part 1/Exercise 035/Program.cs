2e4f7595e531ef7151fbb56c98286a4f9fd95b64
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-17 12:19:55 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 035/Program.cs
+++ Exercises/Part 1/Exercise 035/Program.cs
@@ -6,8 +6,16 @@ namespace exercise_35
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        while (true) 
+        {
+            Console.WriteLine("Give a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
+            if ( num1 == 42 ) 
+            {
+                break;
+            }
+        }
     }
   }
 }
