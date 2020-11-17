b49c26d8ce6fc22655bda7b03be04f29a0e6d647
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-17 14:32:02 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 037/Program.cs
+++ Exercises/Part 1/Exercise 037/Program.cs
@@ -6,8 +6,22 @@ namespace exercise_37
   {
     public static void Main(string[] args)
     {
-      int sum = 0;
-      // Write your code here:
+            int sum = 0;
+    
+            while (true) 
+            {
+            Console.WriteLine("Give a number");
+            int number = Convert.ToInt32(Console.ReadLine());
+            if (number == 0) 
+            {
+                break;
+            }
+            if  (number > 0)
+            {
+            sum = sum + 1;
+    }
+}
+            Console.WriteLine("Total amount of numbers: " + sum);
 
     }
   }
