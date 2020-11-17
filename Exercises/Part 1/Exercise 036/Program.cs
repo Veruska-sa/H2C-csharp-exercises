0e27d47fef43bc6a140d6c5f13db20e44f2c6a64
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-17 12:29:09 +0200

    17.11.2020

--- Exercises/Part 1/Exercise 036/Program.cs
+++ Exercises/Part 1/Exercise 036/Program.cs
@@ -6,8 +6,27 @@ namespace exercise_36
   {
     public static void Main(string[] args)
     {
-      // Write your code here:
+        while (true) 
+        {
+            Console.WriteLine("Give a number ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
 
+            if ( num1 == 0 ) 
+            {
+                break;
+            }
+
+            if ( num1 < 0)
+            {
+                Console.WriteLine("That is negative");
+            }
+
+            if (num1 > 0)
+            {
+                int multiply = num1 * num1;
+                Console.WriteLine( multiply );   
+            }
+        }
     }
   }
 }
