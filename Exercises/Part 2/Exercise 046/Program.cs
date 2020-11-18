d9ee8d15f41c77f7248f92efe12c1ccb18a4edd3
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-18 13:55:40 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 046/Program.cs
+++ Exercises/Part 2/Exercise 046/Program.cs
@@ -8,6 +8,13 @@ namespace exercise_46
     {
 
       // Write your code here:
+              Console.WriteLine("Give a number: ");
+                int input = Convert.ToInt32(Console.ReadLine());
+                while (100 >= input)
+                {
+                Console.WriteLine(input);
+                input++;
+                }
 
     }
   }
