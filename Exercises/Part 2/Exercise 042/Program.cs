454454a93286a9aa32023232d854673cbdd7ae2f
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-18 11:48:30 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 042/Program.cs
+++ Exercises/Part 2/Exercise 042/Program.cs
@@ -7,8 +7,13 @@ namespace exercise_42
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-    
+                Console.WriteLine("Give an integer");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Give second integer");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int sum = num1 + num2;
+                double squareRoot = Math.Sqrt(sum);
+                Console.WriteLine(squareRoot);
     }
   }
 }
