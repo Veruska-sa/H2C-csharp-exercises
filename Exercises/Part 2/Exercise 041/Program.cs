60b2187bffa084adcd279dd429e45766114debfb
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-18 11:15:55 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 041/Program.cs
+++ Exercises/Part 2/Exercise 041/Program.cs
@@ -7,8 +7,9 @@ namespace exercise_41
     public static void Main(string[] args)
     {
 
-      // Write your code here:
-      
+                Console.WriteLine("Give an integer");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine( num1 * num1 );
     }
   }
 }
