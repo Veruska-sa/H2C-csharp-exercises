6e8bba75d4c27607f0767877c11a014798a0abcf
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-18 22:25:36 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 048/Program.cs
+++ Exercises/Part 2/Exercise 048/Program.cs
@@ -6,9 +6,57 @@ namespace exercise_48
   {
     public static void Main(string[] args)
     {
+            int sum1 = 0;
+            int sum2 = 0;
+            int sum3 = 0;
+            int sum4 = 0;
 
-      // Write your code here:
- 
+            Console.WriteLine("Write numbers:");
+            while (true)
+            {
+                int num1 = Convert.ToInt32(Console.ReadLine());
+            
+            if (num1 == -1)
+            {
+                Console.WriteLine("Thx! Bye!");
+                break;
+            }
+            //Breaks
+
+            if (num1 != -1)
+            {
+                sum1 = sum1 + num1;
+            }
+            //Sum of the given numbers
+            if  (num1 != -1)
+            {
+                sum2 = sum2 + 1;
+            }
+            //How many numbers
+
+            if(num1 % 2 == 0)
+            {
+                 sum3 = sum3 + 1;
+            }
+            //How many even
+
+            if(num1 % 2 != 0)
+            {
+                 sum4 = sum4 + 1;
+            }
+            //How many odd
+
+            }
+
+
+            Console.WriteLine("Sum: " + sum1);
+            Console.WriteLine("Numbers: " + sum2);
+            Console.WriteLine("Average: " + ((double)sum1/sum2));
+            Console.WriteLine("Even: " + sum3);
+            Console.WriteLine("Odd: " + sum4);
+
+            
+
     }
   }
 }
