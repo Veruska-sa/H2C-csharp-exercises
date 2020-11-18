523fe88b7e18bc6e07c898cab24d5d1ddec1383c
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-18 14:36:48 +0200

    18.11.2020

--- Exercises/Part 2/Exercise 047/Program.cs
+++ Exercises/Part 2/Exercise 047/Program.cs
@@ -7,8 +7,15 @@ namespace exercise_47
     public static void Main(string[] args)
     {
 
-      // Write your code here:
       
+            Console.WriteLine("Where to?");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Where from?");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            for (int i = num2; i <= num1; i++) 
+            {
+                Console.WriteLine(i);
+            }
     }
   }
 }
