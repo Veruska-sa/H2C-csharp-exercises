cff9319fa3f4af0dcd4ee71a461a6cbc9caa6b5b
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-25 14:36:48 +0200

    25.11.2020

--- Exercises/Part 2/Exercise 053/Program.cs
+++ Exercises/Part 2/Exercise 053/Program.cs
@@ -3,22 +3,19 @@
 namespace exercise_53
 {
   class Program
-  {
+  { //Main program
     public static void Main(String[] args)
     {
             int numerator = Convert.ToInt32(Console.ReadLine());
             int denominator = Convert.ToInt32(Console.ReadLine());
-            double resultdiv = Division( numerator, denominator);
-            Console.WriteLine(resultdiv);
-            
-
-
+            Division(numerator, denominator);
     }
 
-    public static double Division(int numerator, int denominator)
+        //Method
+    public static void Division(int numerator, int denominator)
     {
-        double result = ((double)numerator/(double)denominator);
-        return result;
+            double result = ((double)numerator/(double)denominator);
+            Console.WriteLine(result);
     }
 
   }
