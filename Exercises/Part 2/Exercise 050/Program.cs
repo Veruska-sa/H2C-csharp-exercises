0b58c0b73721b87ba78805c1dc41bb88a7989eb6
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-25 10:37:36 +0200

    25.11.2020

--- Exercises/Part 2/Exercise 050/Program.cs
+++ Exercises/Part 2/Exercise 050/Program.cs
@@ -4,28 +4,22 @@ namespace exercise_50
 {
   class Program
   {
-    public static void Main(string[] args)
-    {
-      // Call your method here:
+      public static void Main(string[] args)
+       {
+            Console.WriteLine("How many times?");
+            int i = 0;
+            int userInput = Convert.ToInt32(Console.ReadLine());
+            while (i < userInput)
+            {
+                PrintPharse();
+                i++;
+            }
+        }
 
-        Console.WriteLine("How many times?");
-        int userInput = Convert.ToInt32(Console.ReadLine());
-    }
-
-    // Write your method here:
-    public static void PrintPharse(int numOfTimes) 
-{
-    int i = 0;
-    while (i < numOfTimes) 
+        
+        public static void PrintPharse()
     {
-      PrintPharse;
-      i++;
-    }
-}
-
-    public static void PrintPharse()
-    {
-       Console.WriteLine("In a hole in the ground there lived a method");
+        Console.WriteLine("In a hole in the ground there lived a method");
     }
   }
 }
