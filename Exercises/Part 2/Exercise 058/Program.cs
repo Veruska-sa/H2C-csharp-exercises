5290c91a1bed40ba4474843b9b67ba413ef0617e
Vera Saarinen <vera.saarinen@gmail.com>
2020-11-26 14:00:34 +0200

    26.11.2020

--- Exercises/Part 2/Exercise 058/Program.cs
+++ Exercises/Part 2/Exercise 058/Program.cs
@@ -6,11 +6,21 @@ namespace exercise_58
   {
     public static void Main(String[] args)
     {
-      int answer = Smallest(2, 7);
-      Console.WriteLine("Smallest: " + answer);
+        int answer = Smallest(2, 7);
+        Console.WriteLine("Smallest: " + answer);
     }
 
-    // Write your method here:
+    public static int Smallest(int number1, int number2) 
+    {
+              int result;
+                if (number1 < number2)
+                result = number1;
+                else
+                result = number2;
+
+                return result;
+
+    }
     
   }
 }
