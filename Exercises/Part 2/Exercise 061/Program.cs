297c21a8fe7627ae7d5148c7b8628f8d1df647f3
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-02 12:14:37 +0200

    1515

--- Exercises/Part 2/Exercise 061/Program.cs
+++ Exercises/Part 2/Exercise 061/Program.cs
@@ -6,27 +6,46 @@ namespace exercise_61
   {
     public static void Main(String[] args)
     {
-
+            PrintStars(1);
+            PrintSpaces(1);
+            PrintRightTriangle(4);
     }
 
     public static void PrintStars(int number)
     {
+            int i = 0;
 
+            while (i < number)
+            {
+                Console.Write("*");
+                i++;
+            }
+            Console.WriteLine("");
     }
 
     public static void PrintSpaces(int number)
     {
+            int i = 0;
 
+            while (i < number)
+            {
+                Console.Write("");
+                i++;
+            }
     }
 
     public static void PrintRightTriangle(int size)
     {
-
+            for (int i = 1; i <=size; i++)
+            {
+                PrintSpaces(size-i);
+                PrintStars(i);
+            }
     }
 
-    public static void ChristmasTree(int height)
-    {
+    //public static void ChristmasTree(int height)
+    //{
 
-    }
+    //}
   }
 }
