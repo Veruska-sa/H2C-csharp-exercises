771673e9ae608c15ab82dddd518fb01822f66dab
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-10 14:22:08 +0200

    10.12.2020

--- Exercises/Part 3/Exercise 087/Program.cs
+++ Exercises/Part 3/Exercise 087/Program.cs
@@ -7,7 +7,16 @@ namespace exercise_87
   {
     public static void Main(string[] args)
     {
+        string userinput = Console.ReadLine();
+        string[] pieces = userinput.Split(' ');
 
+            for (int i = 0; i < pieces.Length; i++)
+            {
+                if (pieces[i].Contains("av"))
+                    {
+                    Console.WriteLine(pieces[i]);
+                    }
+            }
     }
 
   }
