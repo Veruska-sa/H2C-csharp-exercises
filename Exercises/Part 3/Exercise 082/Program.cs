16d166a24aa84c7dd53f9a6f92aedb3864bc41c8
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-10 14:07:40 +0200

    10.12.2020

--- Exercises/Part 3/Exercise 082/Program.cs
+++ Exercises/Part 3/Exercise 082/Program.cs
@@ -32,6 +32,6 @@ namespace exercise_82
 
 
   }
-        }
-    }
+ }
+}
 
--- Exercises/Part 3/Exercise 085/Program.cs
+++ Exercises/Part 3/Exercise 085/Program.cs
@@ -7,6 +7,34 @@ namespace exercise_85
   {
     public static void Main(string[] args)
     {
+            string user1 = "alex sunshine";
+            string[] pieces1 = user1.Split(' ');
+            Console.WriteLine(pieces1[0]);
+            Console.WriteLine(pieces1[1]);
+
+            string user2 = "emma haskell";
+            string[] pieces2 = user2.Split(' ');
+            Console.WriteLine(pieces2[0]);
+            Console.WriteLine(pieces2[1]);
+
+            Console.WriteLine("Enter username:");
+            string usernameinput = Console.ReadLine();
+            Console.WriteLine("Enter password:");
+            string userpasswordinput = Console.ReadLine();
+
+            if(pieces1[0] + pieces1[1] == usernameinput + userpasswordinput)
+                {
+                Console.WriteLine("You have succesfully logged in!");
+                }
+
+            else if(pieces2[0] + pieces2[1] == usernameinput + userpasswordinput)
+                {
+                Console.WriteLine("You have succesfully logged in!");
+                }
+            else
+                {
+                Console.WriteLine("incorrect username or password!");
+                }
 
 
     }
