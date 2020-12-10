404ee4013a903a15b512911f2c83967370960628
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-10 14:14:35 +0200

    10.12.2020

--- Exercises/Part 3/Exercise 086/Program.cs
+++ Exercises/Part 3/Exercise 086/Program.cs
@@ -7,6 +7,14 @@ namespace exercise_86
   {
     public static void Main(string[] args)
     {
+            Console.WriteLine("Give a string");
+            string userstring = Console.ReadLine();
+            string[] pieces = userstring.Split(' ');
+
+            for (int i = 0; i < pieces.Length; i++)
+            {
+            Console.WriteLine(pieces[i]);
+            }
 
 
     }
