a811885e3cff5883efc11f97d62f78b3d89df12c
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-11 14:52:44 +0200

    11.12.2020

--- Exercises/Part 3/Exercise 089/Program.cs
+++ Exercises/Part 3/Exercise 089/Program.cs
@@ -7,11 +7,15 @@ namespace exercise_89
   {
     public static void Main(string[] args)
     {
+            string userstring = Console.ReadLine();
+            string[] pieces = userstring.Split(' ');
+            int pituus = pieces.Length;
+            Console.WriteLine(pieces[pituus-1]);
 
-
+        }
     }
   }
-}
+
 
 
 
