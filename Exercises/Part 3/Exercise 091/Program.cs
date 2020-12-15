d830074b067ebc942ae9d2bed7227b15e9f6d37c
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-15 13:23:04 +0200

    15.12.2020

--- Exercises/Part 3/Exercise 091/Program.cs
+++ Exercises/Part 3/Exercise 091/Program.cs
@@ -7,10 +7,35 @@ namespace exercise_91
   {
     public static void Main(string[] args)
     {
-
-    }
-  }
+        Console.WriteLine("Give name, age (empty line exits):");
+ 
+        int oldest = 0;
+        string oldestName = "";
+        while (true)
+        {
+            string input = Console.ReadLine();
+ 
+        if (input != "")
+           {
+        string[]nameAndAge = input.Split(',');
+        int age = Convert.ToInt32(nameAndAge[1]);
+ 
+        if (age > oldest)
+        {
+        oldest = age;
+        oldestName = nameAndAge[0];
+        }
+ 
+        }
+        else
+        {
+        Console.WriteLine("Name of the oldest: " +oldestName);
+        break;
+        }
+ 
+ }
+ }
+ } 
 }
-
 
 
