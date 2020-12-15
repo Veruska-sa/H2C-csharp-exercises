c89b41a2e5e90a8c40fd9616a2c5477662284a47
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-15 10:47:12 +0200

    15.12.2020

--- Exercises/Part 3/Exercise 092/Program.cs
+++ Exercises/Part 3/Exercise 092/Program.cs
@@ -7,6 +7,35 @@ namespace exercise_92
   {
     public static void Main(string[] args)
     {
+            int highest = 0;
+            int longest = 0;
+            string longestName = "";
+            while (true)
+                {
+                string input = Console.ReadLine();
+                if (input != "")
+                    {
+                    string [] nameAndAge = input.Split(',');
+                    int year = Convert.ToInt32(nameAndAge[1]);
+                    int nameSize = nameAndAge[0].Length;
+                    int age = 2020 -(year);
+
+                    if (age > highest) highest = age;
+
+                        if (nameSize > longest)
+                        {
+                        longest = nameSize;
+                        longestName = nameAndAge[0];
+                        }
+                }
+
+                else
+                {
+                    Console.WriteLine("Longest name: " + longestName);
+                    Console.WriteLine("Highest age: " + highest);
+                    break;
+                }
+            }
 
     }
   }
