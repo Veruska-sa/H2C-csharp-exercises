34577ea096a4d8184a21116380ce54d0da1d2591
Vera Saarinen <vera.saarinen@gmail.com>
2020-12-15 14:23:01 +0200

    15.12.2020

--- Exercises/Part 1/Exercise 016/Program.cs
+++ Exercises/Part 1/Exercise 016/Program.cs
@@ -6,7 +6,7 @@ namespace exercise_16
   {
     public static void Main(string[] args)
     {
-     Console.Write("How many days? ");
+     Console.WriteLine("How many days? ");
      int  num1 = Convert.ToInt32(Console.ReadLine());
      int result = num1* 24 * 60 * 60;
      Console.WriteLine( result );
